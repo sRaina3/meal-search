11395cef9c1b968491d2ccf2d6d34dddb9c226c5
Saransh Raina <saranshraina1@gmail.com>
2023-05-07 21:01:58 -0500

    Added meal type selection

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React, { useState } from 'react';
+
 import './App.css'
 
 const App = () => {
+  const [mealType, setMealType] = useState('');
+
+  const handleOptionClick = (event) => {
+    setMealType(event.target.innerText);
+  };
+
   return (
     <div className='container'>
       <h1 className='title'>What would you like to Order?</h1>
+      <div className="options-container">
+        <button className={`option ${mealType === 'Breakfast' ? 'selected' : ''}`} onClick={handleOptionClick}>Breakfast</button>
+        <button className={`option ${mealType === 'Lunch' ? 'selected' : ''}`} onClick={handleOptionClick}>Lunch</button>
+        <button className={`option ${mealType === 'Dinner' ? 'selected' : ''}`} onClick={handleOptionClick}>Dinner</button>
+      </div>
     </div>
   )
 }
